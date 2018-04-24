fc8fb0ec027731e536386bffb1dae8197acdd0cb
wley3337@gmail.com <wley3337@gmail.com>
2018-04-24 02:00:31 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
-def fizzbuzz(num)
-  if(num/3)
-      if(num/5)
+def fizzbuzz(new_num)
+  if(new_num/3)
+      if(new_num/5)
         "Fizzbuzz"
       else
         "Fizz"
       end
-  elsif (num/5)
+  elsif (new_num/5)
     "Buzz"
   else 
     nil
